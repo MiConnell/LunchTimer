336d096f717589e6fe4eb923cb51201e400c7399
Michael Connell <connellmp@gmail.com>
2020-08-24 10:56:07 -0700

    adding icon

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import rumps
 import time
-import datetime
 
 class LunchTimer(rumps.App):
 
     @rumps.clicked("Start Timer")
     def sayhi(self, _):
         time.sleep(3)
-        rumps.notification("Timer Complete", "Time to clock back in!", "It has been 29 minutes, get ready!")
+        rumps.notification("Timer Complete", "Time to clock back in!", "It has been 29 minutes, get ready to clock in!")
 
 if __name__ == "__main__":
-    LunchTimer("Awesome App").run()
+    app = LunchTimer("Lunch Timer", icon='./timer.png')
+    # Icon made by Freepik (https://www.flaticon.com/authors/freepik) from Flaticon (https://www.flaticon.com/)
+    app.run()
 
